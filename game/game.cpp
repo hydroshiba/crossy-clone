4c35c5417747da8637d323754ded8e8a558e8007
Minh Phan Hải <36961737+hydroshiba@users.noreply.github.com>
2023-10-24 23:19:07 +0700

    Disable scrolling & remove cursor

--- game/game.cpp
+++ game/game.cpp
@@ -2,21 +2,37 @@
 #include <iostream>
 
 Game::Game() {
+    // Get console handle & device context
     console = GetConsoleWindow();
     hdc = GetDC(console);
 
+    // Set title
     SetConsoleTitle("Crossy Clone");
+    
+    // Maximize window & disable resizing
     ShowWindow(console, SW_MAXIMIZE);
-
     DWORD style = GetWindowLong(console, GWL_STYLE);
     style &= ~WS_MAXIMIZEBOX;
     SetWindowLong(console, GWL_STYLE, style);
 
+    // Get window size
     RECT size;
     GetWindowRect(console, &size);
     width = size.right - size.left;
     height = size.bottom - size.top;
 
+    // Remove cursor
+    HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO cursorInfo;
+
+    GetConsoleCursorInfo(out, &cursorInfo);
+    cursorInfo.bVisible = false; // Set the cursor visibility
+    SetConsoleCursorInfo(out, &cursorInfo);
+
+    // Disable scrolling
+    ShowScrollBar(GetConsoleWindow(), SB_VERT, 0);
+
+    // Create engine
     engine = new Engine(hdc, width, height);
 }
 
@@ -27,7 +43,7 @@ void Game::run() {
     while (true) {
         for (int x = 0; x < width; x++)
             for (int y = 0; y < height; y++)
-                engine->set(x, y, count[0], count[1], count[2]);
+                engine->set(x, y, count[2], count[1], count[0]);
 
         if (count[cur] == 0 || count[cur] == 255) {
 			cur += numcur;
