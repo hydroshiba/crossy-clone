6f06bf2a1a7d42f0b67ebc959e420d4846742b05
22127433 <anhtus2711@gmail.com>
2023-12-01 15:39:15 +0700

    Demo leaderboard

--- source/scene/leaderboard.cpp
+++ source/scene/leaderboard.cpp
@@ -1,27 +1,33 @@
 #include "leaderboard.hpp"
 
-Leaderboard::Leaderboard(int width, int height, Engine* engine, Speaker* speaker, SceneRegistry* registry, Setting* setting, Keyboard* keyboard) : 
-    Scene(width, height, engine, speaker, registry, setting, keyboard) {}
+Leaderboard::Leaderboard(int width, int height, Engine* engine, Speaker* speaker, SceneRegistry* registry, Setting* setting, Keyboard* keyboard) : Scene(width, height, engine, speaker, registry, setting, keyboard),
+                                                                                                                                                 GOLD("asset/texture/cup/gold.bmp"),
+                                                                                                                                                 SILVER("asset/texture/cup/silver.bmp"),
+                                                                                                                                                 COPPER("asset/texture/cup/copper.bmp"),
+                                                                                                                                                 gold((width - GOLD.getWidth()) / 2, (height - GOLD.getHeight()) / 3, GOLD),
+                                                                                                                                                 silver((width - SILVER.getWidth()) / 2 - 400, (height - SILVER.getHeight()) / 3 + 180, SILVER),
+                                                                                                                                                 copper((width - COPPER.getWidth()) / 2 + 400, (height - COPPER.getHeight()) / 3 + 180, COPPER) 
+                                                                                                                                                  {}
 
 Leaderboard::~Leaderboard() {
 }
 
 Scene* Leaderboard::process() {
-    bool isExit = false;
+    Scene* next = this;
+    Key pressedKey = keyboard->key();
 
-    while(!isExit) {
-        keyboard->refresh();
-        Key pressedKey = keyboard->key();
-
-        if(pressedKey == Key::ESC) {
-            isExit = true;
-        }
+    if(pressedKey == Key::ESC) {
+        next = sceneRegistry->scene(SceneID::MENU);
     }
 
-    return sceneRegistry->scene(SceneID::MENU);
+    return next;
 }
 
 void Leaderboard::render() {
+    engine->fill(0, 162, 232);
+    gold.render(engine);
+    silver.render(engine);
+    copper.render(engine);
 }
 
 void Leaderboard::playsound() {
