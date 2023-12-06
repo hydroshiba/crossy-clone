a857bc5fa145bb96e33534bfca2996b188f6f751
hydroshiba <36961737+hydroshiba@users.noreply.github.com>
2023-12-06 15:36:03 +0700

    Delete redundant headers in main

--- source/main.cpp
+++ source/main.cpp
@@ -1,17 +1,4 @@
-#include <windows.h>
-#include <filesystem>
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-
-#include "resource.hpp"
 #include "game.hpp"
-#include "scene.hpp"
-#include "player.hpp"
-#include "sound.hpp"
-
-#include <mmsystem.h>
-#include <vector>
 
 int main() {
     Game game;
