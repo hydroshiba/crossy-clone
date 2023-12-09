c79eed7490f247a9ba32f4d3eefcda186be7972a
22127433 <anhtus2711@gmail.com>
2023-12-09 14:14:55 +0700

    Update textbox

--- source/object/textbox.cpp
+++ source/object/textbox.cpp
@@ -50,42 +50,110 @@ Textbox::~Textbox() {
     }
 }
 
-void Textbox::setText(std::string textStr, float x, float y, float ratioX, float ratioY) {
-    //Delete old text
-    for (Isometric* i : text) {
-        delete i;
+Vec2 Textbox::getOffset3D(int idxChar) {
+    if(idxChar >= text.size()){
+        return {0.0f, 0.0f};
     }
-    text.clear();
+    if(text[idxChar] == NULL){
+        return  {x + idxChar * FONT[0].getWidth() * ratioX, y + idxChar * FONT[0].getHeight() * ratioY + idxY * FONT[0].getHeight()};
+    }
+    return {float(text[idxChar]->getX()), float(text[idxChar]->getY())};
+}
 
-    //Create new text
-    float idx = 0.0f;
+void Textbox::replaceChar(char c, int idx) {
+    if(idx > text.size()){
+        return;
+    }
+    Vec2 offset = getOffset3D(idx);
+    delete text[idx];
+    if(c == ' '){
+        text[idx] = NULL;
+        return;
+    }
+
+    if(c >= '0' && c <= '9'){
+        float w = FONT[c - '0'].getWidth();
+        float h = FONT[c - '0'].getHeight();
+        text[idx] = new Isometric(FONT[c - '0'], {w, h}, {0.0f, 0.0f}, {offset.x, offset.y});
+        return;
+    }
+    if(!isalpha(c)){
+        return;
+    }
+    float w = FONT[c - 'A' + 10].getWidth();
+    float h = FONT[c - 'A' + 10].getHeight();
+    text[idx] = new Isometric(FONT[c - 'A' + 10], {w, h}, {0.0f, 0.0f}, {offset.x, offset.y});
+
+}
+
+void Textbox::addText(std::string textStr) {
     float w = 1, h = 1;
     for(char c : textStr){
         if(c == ' '){
-            idx++;
+            idxX++;
+            text.push_back(NULL);
             continue;
         }
         if(c == '\n'){
-            idx = 0.0f;
-            y += 1.2 * h;
+            idxX = 0.0f;
+            idxY++;
             continue;
         }
         if(c >= '0' && c <= '9'){
             w = FONT[c - '0'].getWidth();
             h = FONT[c - '0'].getHeight();
-            text.push_back(new Isometric(FONT[c - '0'], {w, h}, {0.0f, 0.0f}, {x + idx * w * ratioX, y + idx * h * ratioY}));
+            text.push_back(new Isometric(FONT[c - '0'], {w, h}, {0.0f, 0.0f}, {x + idxX * w * ratioX, y + idxX * h * ratioY + idxY * h}));
         } else if(isalpha(c)){
             c = toupper(c);
             w = FONT[c - 'A' + 10].getWidth();
             h = FONT[c - 'A' + 10].getHeight();
-            text.push_back(new Isometric(FONT[c - 'A' + 10], {w, h}, {0.0f, 0.0f}, {x + idx * w * ratioX, y + idx * h * ratioY}));
+            text.push_back(new Isometric(FONT[c - 'A' + 10], {w, h}, {0.0f, 0.0f}, {x + idxX * w * ratioX, y + idxX * h * ratioY + idxY * h}));
         }
-        idx++;
+        idxX++;
+    }
+}
+
+void Textbox::setText(std::string textStr, float x, float y, float ratioX, float ratioY) {
+    //Delete old text
+    for (Isometric* i : text) {
+        if(i != NULL) delete i;
+    }
+    text.clear();
+
+    //Create new text
+    this->x = x;
+    this->y = y;
+    this->ratioX = ratioX;
+    this->ratioY = ratioY;
+    idxX = idxY = 0.0f;
+    float w = 1, h = 1;
+    for(char c : textStr){
+        if(c == ' '){
+            idxX++;
+            text.push_back(NULL);
+            continue;
+        }
+        if(c == '\n'){
+            idxX = 0.0f;
+            idxY++;
+            continue;
+        }
+        if(c >= '0' && c <= '9'){
+            w = FONT[c - '0'].getWidth();
+            h = FONT[c - '0'].getHeight();
+            text.push_back(new Isometric(FONT[c - '0'], {w, h}, {0.0f, 0.0f}, {x + idxX * w * ratioX, y + idxX * h * ratioY + idxY * h}));
+        } else if(isalpha(c)){
+            c = toupper(c);
+            w = FONT[c - 'A' + 10].getWidth();
+            h = FONT[c - 'A' + 10].getHeight();
+            text.push_back(new Isometric(FONT[c - 'A' + 10], {w, h}, {0.0f, 0.0f}, {x + idxX * w * ratioX, y + idxX * h * ratioY + idxY * h}));
+        }
+        idxX++;
     }
 }
 
 void Textbox::render(Engine* engine) {
     for (Isometric* i : text) {
-        i->render(engine);
+        if(i != NULL) i->render(engine);
     }
 }
--- source/object/textbox.hpp
+++ source/object/textbox.hpp
@@ -5,12 +5,19 @@
 #include "isometric.hpp"
 
 class Textbox {
+    float x, y;
+    float idxX;
+    float idxY;
+    float ratioX, ratioY;
     std::vector<Isometric*> text;
     static std::vector<Texture> FONT;
 public:
     Textbox(std::string textStr, float x, float y, float ratioX, float ratioY);
     ~Textbox();
 
+    Vec2 getOffset3D(int idxChar);
+    void replaceChar(char c, int idx);
+    void addText(std::string textStr);
     void setText(std::string textStr, float x, float y, float ratioX, float ratioY);
     void render(Engine* engine);
 };
